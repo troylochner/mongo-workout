5f31142cf17fb3be69d8357cc45cc387260de02d
TroyL <troy.lochner@gmail.com>
2020-12-05 17:37:07 -0500

    Heroku setup applied

--- server.js
+++ server.js
@@ -13,10 +13,23 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+/*
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
-});
+});*/
+
+//HEROKU SETUP
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 // routes
 app.use(require("./routes/api.js"));
